9dcf7e7d847190e6b707c340f93f32e19822216f
sulemanbasit <sulemanugrad@gmail.com>
2025-03-10 00:35:37 -0600

    Major updates to before and after execution scripts

--- after_execution.sh
+++ after_execution.sh
@@ -2,12 +2,20 @@
 
 rm -dr *data
 
-destination="/Users/dianbasit/Dropbox/Investments/Prop Farming/Reports/log"
-base="/Users/dianbasit/Dropbox/Investments/Prop Farming/EAs/Dian-EAs/GitHub Data/Combined_CSV_FOREX"
+# destination="/Users/dianbasit/Dropbox/Investments/Prop Farming/Reports/log"
+# base="/Users/dianbasit/Dropbox/Investments/Prop Farming/EAs/Dian-EAs/GitHub Data/Combined_CSV_FOREX"
+destination="/Users/sulemanbasit/Dropbox/Investments/Prop Farming/Reports/log"
+base="/Users/sulemanbasit/Dropbox/Investments/Prop Farming/EAs/Dian-EAs/GitHub Data/Combined_CSV_FOREX"
 
 for input in [0-9]*; do
-if [[ -f $input ]]; then
-mv ${input:0:4}* "$destination"               
-fi
+    if [[ -f $input ]]; then
+        destination_data_folder=$(find "$destination" -type d -name "${input:0:4}*" | head -n 1) # Find the folder matching the pattern
+        echo $destination_data_folder
+        mv ${input:0:4}* "$destination_data_folder/"        
+        mv "$destination_data_folder"/*log* "$destination_data_folder/Archive/"
+    fi
+
 done
 
+cd "/Users/sulemanbasit/Dropbox/Investments/Prop Farming/Reports/log/"
+bash "/Users/sulemanbasit/Dropbox/Investments/Prop Farming/Reports/log/log2archive.sh"
--- before_execution.sh
+++ before_execution.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-destination="/Users/dianbasit/Dropbox/Investments/Prop Farming/Reports/log"
-base="/Users/dianbasit/Dropbox/Investments/Prop Farming/EAs/Dian-EAs/GitHub Data/Combined_CSV_FOREX"
+# shifts entire log files from misc to logs specific directory
+cd "/Users/sulemanbasit/Dropbox/Investments/Prop Farming/Reports/log/"
+bash "/Users/sulemanbasit/Dropbox/Investments/Prop Farming/Reports/log/misc2log.sh"
+
+# destination="/Users/dianbasit/Dropbox/Investments/Prop Farming/Reports/log"
+# base="/Users/dianbasit/Dropbox/Investments/Prop Farming/EAs/Dian-EAs/GitHub Data/Combined_CSV_FOREX"
+destination="/Users/sulemanbasit/Dropbox/Investments/Prop Farming/Reports/log"
+base="/Users/sulemanbasit/Dropbox/Investments/Prop Farming/EAs/Dian-EAs/GitHub Data/Combined_CSV_FOREX"
 
 cd "$destination/"
-for folder in [0-9]*/; do 
-mkdir "$base/${folder:0:4}_data"
-find "$folder" -maxdepth 1 -type f -name "*log*" -exec cp {} "$base/${folder:0:4}_data" \;
+for folder in [0-9]*/; do  # picks all the folders
+# for folder in 4169*/; do # picks only specific folder
+#for folder in 3123*/; do # picks only specific folder
+    mkdir "$base/${folder:0:4}_data"
+    find "$folder" -maxdepth 1 -type f -name "*log*" -exec cp {} "$base/${folder:0:4}_data" \;
 
 done
